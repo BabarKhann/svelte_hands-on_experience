2af170d19c8069b956c8d3f4ff8be1ab1cfe29fa
BabarKhann <babarkhan138@gmail.com>
2024-08-01 00:49:43 +0500

    create store/state, update state and localstorage

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,35 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import { get } from "svelte/store";
+  import { state, update } from "./state";
+
+  const addStudent = () => {
+    update((state) => {
+      console.log("### state ###", state);
+      state.students.unshift({
+        id: state.students.length + 1,
+        name: "John Doe",
+      });
+
+      return state;
+    });
+  };
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+  <h2><strong>Reading the state</strong></h2>
+  <pre>State without get: {JSON.stringify(state)}</pre>
+  <pre>State with get: {JSON.stringify(get(state))}</pre>
 
-  <div class="card">
-    <Counter />
-  </div>
+  <hr />
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
+  <h2><strong>Updating the state</strong></h2>
 
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <button on:click={addStudent}>Add</button>
+
+  <!-- render students -->
+  <ul>
+    {#each $state.students as student}
+      <li>#{student.id} : {student.name}</li>
+    {/each}
+  </ul>
 </main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
